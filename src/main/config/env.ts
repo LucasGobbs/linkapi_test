e8959e55d5c809a1adec550692c91ceeae4434c9
LucasGobbs <llcostagobbi@gmail.com>
2021-11-06 15:46:16 -0300

    Adding client tokens of Pipedrive and Bling to enviroment

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,5 +3,7 @@ dotenv.config()
 
 export default {
   port: process.env.PORT || 3010,
-  mongo_url: process.env.MONGO_URL
+  mongo_url: process.env.MONGO_URL,
+  pipedrive_token: process.env.PIPEDRIVE_TOKEN,
+  bling_token: process.env.BLING_TOKEN
 }
