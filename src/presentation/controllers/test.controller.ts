c4270052109c50050bbcd02eb69bff9511b45a0e
LucasGobbs <llcostagobbi@gmail.com>
2021-11-06 15:46:46 -0300

    Testing pipedrive client in test route

--- src/presentation/controllers/test.controller.ts
+++ src/presentation/controllers/test.controller.ts
@@ -1,8 +1,13 @@
+import { PipedriveClient } from '@/infra/clients/pipedrive.client'
 import { ok } from '../helpers/http_helper'
 import { Controller, HttpRequest, HttpResponse } from '../protocols'
 
 export class TestController implements Controller {
+  constructor (private readonly pipedrive: PipedriveClient) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    return ok({ message: 'testando api' })
+    const data = await this.pipedrive.listDeals()
+    // const data2 = await this.pipedrive.listWonDeaals()
+
+    return ok(data)
   }
 }
